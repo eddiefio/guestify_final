9a10eb15ca8aa0d064209103b6e44c9d56bddc39
eddiefio <edo.fiorio.01@gmail.com>
2025-04-25 16:06:02 +0200

    cpommiti

--- src/app/guest/payment/[orderId]/page.tsx
+++ src/app/guest/payment/[orderId]/page.tsx
@@ -1,7 +1,14 @@
 // Questo è un server component (la versione predefinita in Next.js App Router)
 import PaymentClient from './PaymentClient';
 
+type PaymentPageProps = {
+  params: {
+    orderId: string;
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
 // Componente server della pagina
-export default function PaymentPage({ params }: { params: { orderId: string } }) {
+export default function PaymentPage({ params }: PaymentPageProps) {
   return <PaymentClient orderId={params.orderId} />;
 } 
