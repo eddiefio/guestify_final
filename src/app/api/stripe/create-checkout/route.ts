ff051e2b8afb0848fbfc4fb4ae853090c91faeef
eddiefio <edo.fiorio.01@gmail.com>
2025-04-30 14:21:42 +0200

    commit

--- src/app/api/stripe/create-checkout/route.ts
+++ src/app/api/stripe/create-checkout/route.ts
@@ -81,7 +81,7 @@ export async function POST(request: Request) {
         propertyId: propertyId,
       },
       automatic_payment_methods: {
-        enabled: false,
+        enabled: true,
       },
     });
 
