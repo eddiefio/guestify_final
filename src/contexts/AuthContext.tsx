c5f508a0ae59055a3e42b671c321ffe6bd75b387
eddiefio <edo.fiorio.01@gmail.com>
2025-05-22 16:43:55 +0200

    commiittte

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -234,7 +234,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `http://localhost:3000/auth/callback`,
+          redirectTo: `https://app.guestify.shop/auth/callback`,
           queryParams: {
             access_type: 'offline',
             prompt: 'consent',
